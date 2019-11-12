e5604e37da60055b25485317ef6baffd96f159c6
Justin Beringer <jbberinger@gmail.com>
2019-11-11 18:55:13 -0800

    fix infinite loop bug

--- a_star.py
+++ a_star.py
@@ -1,6 +1,8 @@
 import numpy as np
+import sys
 '''
 https://en.wikipedia.org/wiki/A*_search_algorithm
+http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
 A* finds the shortest path from start to goal. It is an extension of Dijkstra's algorithm.
 
 A* finds the patch that minimizes f(n) = g(n) + h(n)
@@ -19,16 +21,24 @@ class Node:
     def __init__(self, parent_node=None, position=None):
         self.parent_node = parent_node
         self.position = position
-        self.f = 0
         self.g = 0
         self.h = 0
+        self.f = 0
 
     def __eq__(self, other):
-        return np.array_equal(self, other)
+        return np.array_equal(self.position, other.position)
 
 
 def reconstruct_path(solution):
-    return solution
+    path = []
+    while solution is not None:
+        path.append(solution.position)
+        solution = solution.parent_node
+    return path[::-1]
+
+# Manhattan distance
+def heuristic_cost(position1, position2):
+    return abs(position1[0] - position2[0]) + abs(position1[1] - position2[1])
 
 def a_star(graph, start_position, goal_position):
     occupied_spaces = graph['occupied_spaces']
@@ -43,17 +53,25 @@ def a_star(graph, start_position, goal_position):
     # nodes that have been fully evluated
     closed_list = []
 
-    while len(open_list) > 0:
+    while open_list:
+
         current_node = open_list[0]
         current_index = 0
+
+        # displays count
+        # i = len(open_list)
+        # j = open_list
+        # sys.stdout.write("\rLoops %i and open list" % i % j)
+        # sys.stdout.flush()
+        print(len(open_list))
 
         # finds open node with lowest f score
         for index, open_node in enumerate(open_list):
             if open_node.f < current_node.f:
                 current_node = open_node
-                curent_index = index
+                current_index = index
 
-        open_list.pop(curent_index)
+        open_list.pop(current_index)
         closed_list.append(current_node)
 
         # return path if solution found
@@ -62,27 +80,36 @@ def a_star(graph, start_position, goal_position):
 
         children = []
         # generates valid child nodes
-        for move in [[0, -1], [0, 1], [-1, 0], [1, 0]]:
-            position = np.add(current_node.position, move)
+        for move in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            new_position = np.add(current_node.position, move)
 
-            # check board bonudaries
-            if position[0] > width - 1 or position[1] > height - 1:
+            # check border boundaries
+            if new_position[0] not in range(0, width) or new_position[1] not in range(0, height):
                 continue
+
             # check if position is occupied
-            if len(filter(lambda x: np.array_equal(position, x)), occupied_spaces) > 0:
+            is_occupied = False
+            for occupied_position in occupied_spaces:
+                if np.array_equal(new_position, occupied_position):
+                    is_occupied = True
+                    break
+            if is_occupied:
                 continue
 
-            new_node = Node(current_node, position)
+            new_node = Node(current_node, new_position)
             children.append(new_node)
 
         for child_node in children:
-
+            skip = False
             for closed_node in closed_list:
                 if child_node == closed_node:
+                    skip = True
                     continue
+            if skip:
+                continue
 
             child_node.g = current_node.g + 1
-            child_node.h = np.linalg.norm(child_node.position, goal_position)
+            child_node.h = heuristic_cost(child_node.position, end_node.position)
             child_node.f = child_node.g + child_node.h
 
             for open_node in open_list:
@@ -90,3 +117,15 @@ def a_star(graph, start_position, goal_position):
                     continue
 
             open_list.append(child_node) 
+
+if __name__ == "__main__":
+    graph = {
+        'occupied_spaces': [[1,0],[1,1],[1,2],[1,3]],
+        'width': 3,
+        'height': 5
+    }
+    start_position = [0,0]
+    goal_position = [2,0]
+
+    path = a_star(graph, start_position, goal_position)
+    print('\n', path)
