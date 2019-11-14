e461db7d4f5a0a81da52dc8473002c24533e1e61
Justin Beringer <jbberinger@gmail.com>
2019-11-13 23:40:08 -0800

    add Profile

--- main.py
+++ main.py
@@ -36,3 +36,8 @@ def ping():
 
 
 bottle.run(host='localhost', port=8080, reloader=True)
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    bottle.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    bottle.run(host='localhost', port=8080, reloader=True, debug=True)
